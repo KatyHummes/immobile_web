77bfd8b0d1bca936455026c6be3affa6ad3d91e9
KatyHummes <katielihummes15@gmail.com>
2024-02-23 17:12:49 -0300

    arrumando design

--- resources/js/Pages/Create.vue
+++ resources/js/Pages/Create.vue
@@ -2,6 +2,7 @@
 import AppLayout from '@/Layouts/AppLayout.vue';
 import InputNumber from 'primevue/inputnumber';
 import Textarea from 'primevue/textarea';
+import InputText from 'primevue/inputtext';
 import { useForm } from 'laravel-precognition-vue-inertia';
 
 
@@ -24,21 +25,21 @@ const CreateSubmit = () => form.submit({
             <form @submit.prevent="CreateSubmit">
                 <div class="grid md:grid-cols-7 gap-4">
                     <div class="mb-4">
-                        <label for="title" class="block text-gray-700 text-sm font-bold mb-2">Título</label>
-                        <input v-model="form.title" type="text" id="title" name="title" class="form-input">
+                        <label for="title" class="font-bold block mb-2">Título</label>
+                        <InputText v-model="form.title" />
                         <div v-if="form.invalid('title')" class="font-semibold text-red-500">
                             {{ form.errors.title }}
                         </div>
                     </div>
                     <div class="mb-4">
                         <label class="font-bold block mb-2">Preço</label>
-                        <InputNumber v-model="form.price" mode="currency" currency="BRL" locale="pt-br" />
+                        <InputNumber v-model="form.price" mode="currency" showButtons currency="BRL" :min="0" :max="100" />
                         <div v-if="form.invalid('price')" class="font-semibold text-red-500">
                             {{ form.errors.price }}
                         </div>
                     </div>
                     <div class="mb-4">
-                        <label for="description" class="block text-sm font-bold mb-2">Descrição</label>
+                        <label for="description" class="font-bold block mb-2">Descrição</label>
                         <Textarea v-model="form.description" rows="5" cols="30" />
                         <div v-if="form.invalid('description')" class="font-semibold text-red-500">
                             {{ form.errors.description }}
